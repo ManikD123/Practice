243aef58e953da85d688ef9ca9e53cafd7738450
ManikD123 <iammanik188@gmail.com>
2025-03-29 11:10:55 +0530

    Event Capturing

--- Ch 6 Machine Coding/prac/script.js
+++ Ch 6 Machine Coding/prac/script.js
@@ -2,12 +2,24 @@ const grandParent = document.querySelector('#grandParent')
 const parent = document.querySelector('#parent')
 const child = document.querySelector('#child')
 
+// grandParent.addEventListener('click', () => {
+//     console.log("Grand parent Click")
+// })
+// parent.addEventListener('click', () => {
+//     console.log("parent Click")
+// })
+// child.addEventListener('click', () => {
+//     console.log(" child Click")
+// })
+
+// Event Capturing
 grandParent.addEventListener('click', () => {
-    console.log("Grand parent Click")
-})
+    console.log("Grand parent Click (Capturing)")
+}, true)
 parent.addEventListener('click', () => {
-    console.log("parent Click")
-})
+    console.log("parent Click (Capturing)")
+}, true)
 child.addEventListener('click', () => {
-    console.log(" child Click")
-})
+    console.log(" child Click (Capturing)")
+}, true)
+
