5c87dcc0389d95451bf74973444780919a311db2
ManikD123 <iammanik188@gmail.com>
2025-08-03 16:44:41 +0530

    Count Down timer

---
+++ Machine Coding/Timer/script.js
@@ -0,0 +1,114 @@
+const startBtn = document.querySelector("#start");
+const hrInput = document.getElementById("hr")
+const minInput = document.getElementById("min")
+const secInput = document.getElementById("sec")
+let counterId = null;
+let saveTimeLeft = null;
+
+const TransformedInput = (hrs, mins, secs) => {
+    if (secs >= 60) {
+        //mins = mins + Math.floor(secs / 60);
+        mins++;
+        secs = secs % 60;
+    }
+    if (mins >= 60) {
+        //hrs = hrs + Math.floor(mins / 60);
+        hrs++;
+        mins = mins % 60;
+    }
+    return {
+        transformedhrs: hrs,
+        transformedmins: mins,
+        transformedsecs: secs
+    };
+};
+
+
+const validatedInput = (hrs, mins, secs) => {
+    if (hrs === 0 && mins === 0 && secs === 0) {
+        alert("Please enter the valid Time")
+        return false;
+    }
+
+    if (hrs > 24 || hrs < 0 || mins < 0 || secs < 0) {
+        alert("negative and hrs > 24 are not allowed");
+        return false;
+    }
+
+    const { transformedhrs,
+        transformedmins,
+        transformedsecs } = TransformedInput(hrs, mins, secs);
+
+    const totalTimeInSecs = transformedhrs * 60 * 60 + transformedmins * 60 + transformedsecs;
+    if (totalTimeInSecs > 86400) {
+        alert("Total Time should not be more than 24 Hours")
+        return false;
+    }
+    return true;
+
+}
+
+const updateUI = (countDownTimer) => {
+    if(countDownTimer === 0){
+        clearInterval(counterId);
+        hrInput.value = "00";
+        minInput.value = "00";
+        secInput.value = "00";
+        return;
+    }
+    // if timer is not0 we will update as
+    let hrs = Math.floor(countDownTimer / 3600);
+    let mins = Math.floor((countDownTimer % 3600) / 60);
+    let secs = countDownTimer % 60;
+    if(secs > 0){
+        secs--;
+        secInput.value = secs < 10 ? `0${secs}` : secs;
+        minInput.value = mins < 10 || mins === 0 ? `${mins}` : mins;
+        hrInput.value = hrs < 10 || hrs === 0 ? `${hrs}` : hrs;
+        return;
+    }
+    if(mins > 0){
+        mins--;
+        minInput.value = mins < 10 ? `0${mins}` : mins;
+        secInput.value = 59;
+        hrInput.value = hrs;
+        return;
+
+    }if(hrs > 0){
+        hrs--;
+        minInput.value = 59;
+        minInput.value = 59;
+        hrInput.value = hrs < 10 ? `0${hrs}` : hrs;
+        return;
+    }
+};
+
+const timer = (countDownTime) => {
+
+    
+  counterId = setInterval(() => {
+    updateUI(countDownTime);
+    countDownTime--;
+    saveTimeLeft = countDownTime;
+  }, 1000);
+};
+
+// User might have entered it in frontend nad validate that the valueis correct
+startBtn.addEventListener("click", () => {
+    const hrs = parseInt(hrInput.value) || 0;
+    const mins = parseInt(minInput.value) || 0;
+    const secs = parseInt(secInput.value) || 0;
+
+    if (!validatedInput(hrs, mins, secs)) {
+        return;
+    }
+    const { transformedhrs, transformedmins, transformedsecs } = TransformedInput(
+    hrs,
+    mins,
+    secs
+  );
+  console.log(transformedhrs, transformedmins, transformedsecs);
+  const totalTimeInSeconds =
+    transformedhrs * 60 * 60 + transformedmins * 60 + transformedsecs;
+  timer(totalTimeInSeconds);
+});
