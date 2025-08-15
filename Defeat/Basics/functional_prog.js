74d533be8287149537e3ba77fff57acb301f63d3
ManikD123 <iammanik188@gmail.com>
2025-08-15 07:50:43 +0530

    defeat functional

---
+++ Defeat/Basics/functional_prog.js
@@ -0,0 +1,50 @@
+/*let myRadiusArray = [1,2,3,4,5];
+
+function circumferance(radius){
+    return 2 * Math.PI * radius;
+}
+
+function area(radius){
+    return Math.PI * radius * radius;
+}
+
+function diameter(radius){
+    return Math.PI * radius;
+}
+
+function calculate( radiusArr, logic ){
+    let ans = [];
+    for( let i = 0; i < radiusArr.length; i++){
+        ans.push(logic(radiusArr[i]));
+    }
+    return ans;
+}
+
+let finalCircumferance = calculate(myRadiusArray,circumferance );
+console.log(finalCircumferance);
+let finalarea = calculate(myRadiusArray,area );
+console.log(finalarea);
+let finalDiameter = calculate(myRadiusArray,diameter );
+console.log(finalDiameter);*/
+
+
+let arr = [1,2,3,4,5];
+/* MAP Function
+let mappe_array = arr.map(function(num){
+    return num * 3;
+})
+console.log(mappe_array)
+
+// FIlter program
+let even_Values = arr.filter(function(num) {
+    return num % 2 === 0;
+})
+console.log(even_Values)
+
+*/
+
+let totalSum = arr.reduce(function(acc, num){
+    acc = acc + num;
+    return acc;
+})
+console.log(totalSum)
