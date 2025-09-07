b254354c514ab972f4d32710fd193aaf5093a326
ManikD123 <iammanik188@gmail.com>
2025-09-07 22:05:02 +0530

    imdb start

---
+++ React/Imdb-Clone/Imdb-Clone/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 class="text-5xl font-bold underline rounded-lg m-5 p-3 bg-gray-600 text-gray-200">
+      Hello world!
+    </h1>);
+}
+
+export default App
