635933862e2385dd6c74e4986b1b1444bffd3dba
ManikD123 <iammanik188@gmail.com>
2025-09-13 15:27:52 +0530

    added watchlist and emoji for favoriute and remove

--- React/Imdb-Clone/Imdb-Clone/src/components/Movie.jsx
+++ React/Imdb-Clone/Imdb-Clone/src/components/Movie.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 import Spinner from './Spinner';
 import Pagination from './Pagination';
+import MovieList from './MovieList';
 
 const Movie = () => {
 
@@ -41,27 +42,11 @@ const Movie = () => {
             <div className='text-2xl font-bold text-center m-5'>
                 Trending Movies
             </div>
-            <div className="flex justify-evenly flex-wrap gap-8">
-                {movies?.length > 0 && movies.map((movie, index) => {
-                    return <div key={index}>
-
-                        <div
-                            style={{
-                                backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`
-                            }}
-                            className='h-[30vh] w-[200px] bg-center bg-cover rounded-xl flex flex-col justify-between items-center'
-                        >
-                            <div className='text-white w-full text-center text-xl p-2 bg-gray-900/70 rounded-xl'>
-                                {movie?.title}
-                            </div>
-                        </div>
-                    </div>
-                })}
-            </div>
-            <Pagination 
-            pageNo={pageNo}
-            handleNext={handleNext}
-            handlePrev={handlePrev} />
+            <MovieList movies={movies} />
+            <Pagination
+                pageNo={pageNo}
+                handleNext={handleNext}
+                handlePrev={handlePrev} />
         </div>
     )}
     </>
