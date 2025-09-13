4da3f4565e959f721b4f07f748017a970bfaa3f9
ManikD123 <iammanik188@gmail.com>
2025-09-13 16:40:03 +0530

    watchlist usecase

---
+++ React/Imdb-Clone/Imdb-Clone/src/components/MovieInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieInfo = ({ movie }) => {
+    return <div className='bg-white rounded-lg shadow-lg p-8 fixed z-50 backdrop-blur-sm top-[25%] w-[10%] max-w-[35%] max-h-[90vh]'>
+        MovieInfo
+    </div>
+}
+
+export default MovieInfo
--- React/Imdb-Clone/Imdb-Clone/src/components/MovieList.jsx
+++ React/Imdb-Clone/Imdb-Clone/src/components/MovieList.jsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import MovieInfo from './MovieInfo';
 
 const MovieList = ({ movies }) => {
 
     const [watchList, setWatchList] = useState([]);
+    const [openModal, setOpenModel] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+
+    useEffect(() => {
+        // page refresh
+        setWatchList(() => {
+            if (localStorage.getItem("ImdbWatchListMovies")) {
+                return JSON.parse(localStorage.getItem("ImdbWatchListMovies"));
+            }
+            return [];
+        })
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("ImdbWatchListMovies", JSON.stringify(watchList)); // 1st set in Local storage and will be called when watchlisrt is changes 
+    }, [watchList])
+
     const checkIfMoviePresentInWatchList = (movie) => {
         return watchList.find((m) => m?.id == movie?.id) ? true : false;
     }
@@ -12,48 +31,59 @@ const MovieList = ({ movies }) => {
             const updatedMovieList = [...prevMovieList, movie];
             return updatedMovieList;
         });
+
     };
 
     const removeFromWatchList = (movie) => {
         setWatchList((prevMovieList) => {
             const filteredWatchList = prevMovieList.filter((m) => m?.id != movie?.id);
             return filteredWatchList;
-        })
-    }
+        });
+    };
 
-    return <div className="flex justify-evenly flex-wrap gap-8">
-        {movies?.length > 0 &&
-            movies.map((movie, index) => {
-                return <div key={index}>
+    const handleOpenModal = (movie) => {
+        setOpenModel(true);
+        setSelectedMovie(movie)
+    };
 
-                    <div
-                        style={{
-                            backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`
-                        }}
-                        className='h-[30vh] w-[200px] bg-center bg-cover rounded-xl flex flex-col justify-between items-center'
-                    >
-                        {checkIfMoviePresentInWatchList(movie) ? (
-                            <div className='m-4 flex justify-center h-8 w-8 items-center rounded-xl bg-gray-900/60'
-                                onClick={() => removeFromWatchList(movie)}
+    return (
+        <>
+            <div className="flex justify-evenly flex-wrap gap-8">
+                {movies?.length > 0 &&
+                    movies.map((movie, index) => {
+                        return <div key={index}>
+
+                            <div
+                                style={{
+                                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`
+                                }}
+                                className='h-[30vh] w-[200px] bg-center bg-cover rounded-xl flex flex-col justify-between items-center  items-end '
                             >
-                                &#10060;
-                            </div>
-                        ) : (
-                            <div className='m-4 flex justify-center h-8 w-8 items-center rounded-xl bg-gray-900/60'
-                                onClick={() => addToWatchList(movie)}
-                            >
-                                &#128525;
-                            </div>
+                                {checkIfMoviePresentInWatchList(movie) ? (
+                                    <div className='m-4 flex justify-center h-8 w-8 items-center rounded-xl bg-gray-900/60 hover:cursor-pointer'
+                                        onClick={() => removeFromWatchList(movie)}
+                                    >
+                                        &#10060;
+                                    </div>
+                                ) : (
+                                    <div className='m-4 flex justify-center h-8 w-8 items-center rounded-xl bg-gray-900/60 hover:cursor-pointer'
+                                        onClick={() => addToWatchList(movie)}
+                                    >
+                                        &#128525;
+                                    </div>
 
-                        )}
-                        <div className='text-white w-full text-center text-xl p-2 bg-gray-900/70 rounded-xl'>
-                            {movie?.title}
+                                )}
+                                <div className='text-white w-full text-center text-xl p-2 bg-gray-900/70 rounded-xl  hover:cursor-pointer'
+                                    onClick={() => handleOpenModal()} >
+                                    {movie?.title}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            })}
-    </div>;
-
-}
+                    })}
+            </div>;
+            {openModal && <MovieInfo movie={selectedMovie} />}
+        </>
+    );
+};
 
 export default MovieList
