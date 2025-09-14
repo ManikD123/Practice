75131d4a41be113d79f0f149684f99499940fd89
ManikD123 <iammanik188@gmail.com>
2025-09-14 15:51:51 +0530

    with genre list

--- React/Imdb-Clone/Imdb-Clone/src/Features/WatchList/WatchListTable.jsx
+++ React/Imdb-Clone/Imdb-Clone/src/Features/WatchList/WatchListTable.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import Genraids from "../Helpers/Genraids"
+import genreids from "../Helpers/Genraids"
 import { ArrowUp, ArrowDown } from 'lucide-react';
 
 const WatchListTable = () => {
 
   const [watchList, setWatchList] = useState([]);
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
+  const [genreList, setGenreList] = useState(["All Genre"]);
+  const [currGenre, setCurrGenre] = useState("All Genre");
+
 
   useEffect(() => {
     const watchListData =
       JSON.parse(localStorage.getItem("ImdbWatchListMovies"));
+    const genreList = new Set(
+      watchListData?.map((movie) => genreids[movie?.genre_ids[0]])
+    );
+    setGenreList(["All Genre", ...genreList]);
     setWatchList(watchListData);
-
   }, [])
 
   const handleAscendingOrderRatings = () => {
@@ -39,7 +45,19 @@ const WatchListTable = () => {
   };
 
   return (
-    <div>
+    <div className='my-10 mz-5'>
+      <div className='flex justify-center'>
+        {genreList?.map((genre, idx) => {
+          return <div
+            key={idx}
+            className={`mx-4 flex justify-center items-center h-[3rem] w-[9rem] font-bold border rounded-xl cursor-pointer ${currGenre === genre ? "bg-blue-400 text-white" : "bg-gray-300"
+              }`}
+            onClick={() => setCurrGenre(genre)}
+          >
+            {genre}
+          </div>
+        })}
+      </div>
       <div className="flex justify-center my-10">
         <input type='text'
           placeholder='Search By Movie Name'
@@ -76,30 +94,37 @@ const WatchListTable = () => {
         <tbody className="divide-y divide-gray-100 border-t border-gray-100">
           {watchList?.length > 0 &&
             watchList.filter((movie) => {
-              return movie?.title
-                .toLowerCase()
-                .trim()
-                .includes(search.toLowerCase());
-            }).map((movie, idx) => {
-              return <tr key={idx}>
-                <td className="flex items-center px-6 py-4 font-normal text-gray-900">
-                  <img
-                    src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-                    alt="Poster of Movie"
-                    className="h-[6rem] w-[10rem] object-cover"
-                  />
-                  <div>{movie?.title}</div>
-                </td>
-                <td className="pl-6 py-4">{movie?.vote_average}</td>
-                <td className="pl-6 py-4">{movie?.popularity}</td>
-                <td className="pl-6 py-4">{Genraids[movie?.genre_ids[0]]}</td>
-                <td className="pl-6 py-4 text-red-500 text-bold hover:cursor-pointer"
-                  onClick={() => removeFromWatchList(movie)}
-                >
-                  REMOVE
-                </td>
-              </tr>;
-            })}
+              return (
+                currGenre === "All Genre" ||
+                genreids[movie.genre_ids[0]] === currGenre
+              );
+            })
+              .filter((movie) => {
+                return movie?.title
+                  .toLowerCase()
+                  .trim()
+                  .includes(search.toLowerCase());
+              })
+              .map((movie, idx) => {
+                return <tr key={idx}>
+                  <td className="flex items-center px-6 py-4 font-normal text-gray-900">
+                    <img
+                      src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+                      alt="Poster of Movie"
+                      className="h-[6rem] w-[10rem] object-cover"
+                    />
+                    <div>{movie?.title}</div>
+                  </td>
+                  <td className="pl-6 py-4">{movie?.vote_average}</td>
+                  <td className="pl-6 py-4">{movie?.popularity}</td>
+                  <td className="pl-6 py-4">{genreids[movie?.genre_ids[0]]}</td>
+                  <td className="pl-6 py-4 text-red-500 text-bold hover:cursor-pointer"
+                    onClick={() => removeFromWatchList(movie)}
+                  >
+                    REMOVE
+                  </td>
+                </tr>;
+              })}
         </tbody>
       </table>
     </div>
